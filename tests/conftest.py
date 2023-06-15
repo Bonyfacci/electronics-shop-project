34f42b83e820424404aeb91f1c945b282b8f4431
Сергей Платонов <platonovpochta@gmail.com>
2023-06-16 00:23:47 +0200

    Homework-5 add keyboard.py, test_keyboard.py
    +classes Keyboard & MixinLanguage
    +tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.item import Item
+from src.keyboard import Keyboard
 from src.phone import Phone
 
 
@@ -12,3 +13,8 @@ def test_item():
 @pytest.fixture
 def test_phone():
     return Phone('Two', 4, 5, 6)
+
+
+@pytest.fixture
+def test_keyboard():
+    return Keyboard('Three', 7, 8)
